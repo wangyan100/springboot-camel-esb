6801860182c8f2ba9ea626d5cebb4d05388ce22a
YanWang <yan.wang-extern@deutschebahn.com>
2018-09-06 09:03:21 +0200

    init

---
+++ src/main/java/org/yw/springbootcamelesb/SpringbootCamelEsbApplication.java
@@ -0,0 +1,12 @@
+package org.yw.springbootcamelesb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCamelEsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCamelEsbApplication.class, args);
+	}
+}
---
+++ src/test/java/org/yw/springbootcamelesb/SpringbootCamelEsbApplicationTests.java
@@ -0,0 +1,16 @@
+package org.yw.springbootcamelesb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootCamelEsbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
