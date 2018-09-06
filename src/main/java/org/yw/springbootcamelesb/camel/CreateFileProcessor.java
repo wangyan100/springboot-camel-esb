e8cb5a4773e6309df1b01c02bf103943ab445937
YanWang <yan.wang-extern@deutschebahn.com>
2018-09-06 11:25:52 +0200

    update

--- src/main/java/org/yw/springbootcamelesb/camel/CreateFileProcessor.java
+++ src/main/java/org/yw/springbootcamelesb/camel/CreateFileProcessor.java
@@ -2,6 +2,7 @@ package org.yw.springbootcamelesb.camel;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
@@ -12,25 +13,28 @@ import org.yw.springbootcamelesb.soap.FileCreationStatus;
 
 public class CreateFileProcessor implements Processor {
 	private static final Logger LOG = LoggerFactory.getLogger(CreateFileProcessor.class);
+
 	public void process(Exchange exchange) throws Exception {
-		FileCreationStatus status=new FileCreationStatus();
+		FileCreationStatus status = new FileCreationStatus();
 		// Get the parameters list which element is the holder.
 		try {
-        MessageContentsList msgList = (MessageContentsList) exchange.getIn().getBody();
-        String fileName = (String) msgList.get(0);
-        String fileContent = (String) msgList.get(1);
-        String createdForPersonName = (String) msgList.get(2);
-        
-        status.setFileName(fileName);
-        status.setCreatedForPersonName(createdForPersonName);
-        createFile(fileName,fileContent);
-        
-        status.setStatus("File is Created Successfully");
-		}catch(Exception e) {
-			LOG.error("erorr happened ",e);
+			MessageContentsList msgList = (MessageContentsList) exchange.getIn().getBody();
+			String fileName = (String) msgList.get(0);
+			String fileContent = (String) msgList.get(1);
+			String createdForPersonName = (String) msgList.get(2);
+
+			status.setFileName(fileName);
+			status.setCreatedForPersonName(createdForPersonName);
+			createFile(fileName, fileContent);
+			status.setCreationTime(new Date());
+			status.setStatus("File is Created Successfully");
+		} catch (Exception e) {
+			status.setStatus("File Creation Failed");
+			LOG.error("erorr happened ", e);
 		}
+		exchange.getOut().setBody(status);
 	}
-	
+
 	private void createFile(String fileName, String text) throws FileNotFoundException {
 		PrintWriter out = new PrintWriter(fileName);
 		out.println(text);
--- src/main/java/org/yw/springbootcamelesb/soap/FileCreationStatus.java
+++ src/main/java/org/yw/springbootcamelesb/soap/FileCreationStatus.java
@@ -15,7 +15,7 @@ public class FileCreationStatus implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private String status;
 	private String fileName;
-	private SimpleDateFormat creationTime;
+	private java.util.Date creationTime;
 	private String createdForPersonName;
 	public String getStatus() {
 		return status;
@@ -29,17 +29,18 @@ public class FileCreationStatus implements Serializable {
 	public void setFileName(String fileName) {
 		this.fileName = fileName;
 	}
-	public SimpleDateFormat getCreationTime() {
-		return creationTime;
-	}
-	public void setCreationTime(SimpleDateFormat creationTime) {
-		this.creationTime = creationTime;
-	}
+	
 	public String getCreatedForPersonName() {
 		return createdForPersonName;
 	}
 	public void setCreatedForPersonName(String createdForPersonName) {
 		this.createdForPersonName = createdForPersonName;
+	}
+	public java.util.Date getCreationTime() {
+		return creationTime;
+	}
+	public void setCreationTime(java.util.Date creationTime) {
+		this.creationTime = creationTime;
 	}
 
 }
