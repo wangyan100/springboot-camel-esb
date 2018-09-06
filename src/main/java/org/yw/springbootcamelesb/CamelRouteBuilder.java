80e80f50e9f446693f901b11f345a5065cd719b3
YanWang <yan.wang-extern@deutschebahn.com>
2018-09-06 16:32:13 +0200

    update

---
+++ src/main/java/org/yw/springbootcamelesb/CamelRouteBuilder.java
@@ -0,0 +1,17 @@
+package org.yw.springbootcamelesb;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+import org.yw.springbootcamelesb.camel.CreateFileProcessor;
+@Component
+public class CamelRouteBuilder extends RouteBuilder {
+
+	private static final String SOAP_ENDPOINT_URI = "cxf://http://localhost:{{soapEndpointPort}}/soap"
+			+ "?serviceClass=org.yw.springbootcamelesb.soap.CreateFileService";
+
+	@Override
+	public void configure() throws Exception {
+		  from(SOAP_ENDPOINT_URI).process(new CreateFileProcessor());
+	}
+
+}
--- src/main/java/org/yw/springbootcamelesb/SpringbootCamelEsbApplication.java
+++ src/main/java/org/yw/springbootcamelesb/SpringbootCamelEsbApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootCamelEsbApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("soapEndpointPort", "9006");
 		SpringApplication.run(SpringbootCamelEsbApplication.class, args);
 	}
 }
