293835cb4882565b5d5e150f51af2430b9b224df
wangyan100 <wyanbox@gmail.com>
2018-09-15 16:21:25 +0200

    update

--- src/main/java/org/yw/springbootcamelesb/CamelRouteBuilder.java
+++ src/main/java/org/yw/springbootcamelesb/CamelRouteBuilder.java
@@ -1,17 +1,24 @@
 package org.yw.springbootcamelesb;
 
+import javax.xml.bind.JAXBContext;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.converter.jaxb.JaxbDataFormat;
 import org.springframework.stereotype.Component;
 import org.yw.springbootcamelesb.file.CreateFileProcessor;
+import org.yw.springbootcamelesb.soap.FileCreationStatus;
 
 @Component
 public class CamelRouteBuilder extends RouteBuilder {
 
-	private static final String SOAP_ENDPOINT_URI = "cxf://http://localhost:{{soapEndpointPort}}/soap"
-			+ "?serviceClass=org.yw.springbootcamelesb.soap.CreateFileService";
+    private static final String SOAP_ENDPOINT_URI = "cxf://http://localhost:{{soapEndpointPort}}/soap"
+            + "?serviceClass=org.yw.springbootcamelesb.soap.CreateFileService";
 
-	@Override
-	public void configure() throws Exception {
-		from(SOAP_ENDPOINT_URI).process(new CreateFileProcessor());
-	}
+    @Override
+    public void configure() throws Exception {
+        JaxbDataFormat xmlDataFormat = new JaxbDataFormat();
+        JAXBContext con = JAXBContext.newInstance(FileCreationStatus.class);
+        xmlDataFormat.setContext(con);
+        from(SOAP_ENDPOINT_URI).process(new CreateFileProcessor())
+                .marshal(xmlDataFormat).to("file:target/output").unmarshal(xmlDataFormat);
+    }
 }
