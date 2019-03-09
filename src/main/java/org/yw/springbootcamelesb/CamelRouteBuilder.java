c9e19d07aecf73a49c758169f991f61b685e2943
wangyan100 <wyanbox@gmail.com>
2019-03-09 12:43:56 +0100

    update

--- src/main/java/org/yw/springbootcamelesb/CamelRouteBuilder.java
+++ src/main/java/org/yw/springbootcamelesb/CamelRouteBuilder.java
@@ -2,8 +2,13 @@ package org.yw.springbootcamelesb;
 
 import javax.xml.bind.JAXBContext;
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.cxf.CxfEndpoint;
 import org.apache.camel.converter.jaxb.JaxbDataFormat;
+import org.apache.camel.model.RouteDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.yw.springbootcamelesb.file.CreateFileProcessor;
@@ -11,7 +16,7 @@ import org.yw.springbootcamelesb.soap.FileCreationStatus;
 
 @Component
 public class CamelRouteBuilder extends RouteBuilder {
-    
+
     public static final String ROUTE_NAME = "SOAP_FILE";
     
     private static final String SOAP_ENDPOINT_URI = "cxf://http://0.0.0.0:{{soap.port}}/soap"
@@ -28,7 +33,16 @@ public class CamelRouteBuilder extends RouteBuilder {
         JaxbDataFormat xmlDataFormat = new JaxbDataFormat();
         JAXBContext con = JAXBContext.newInstance(FileCreationStatus.class);
         xmlDataFormat.setContext(con);
+        /*
         from(SOAP_ENDPOINT_URI).routeId(ROUTE_NAME).process(createFileProcessor).id("createFileProcessor")
                 .marshal(xmlDataFormat).id("marshal").to("file:target/output").unmarshal(xmlDataFormat).id("unmarshal");
+        */
+
+        from(SOAP_ENDPOINT_URI).routeId(ROUTE_NAME).process(createFileProcessor);
+
+
+       //from("file://target/input").log(LoggingLevel.INFO, " file is moved to file  ").to("file://target/output");
+
+       //from("file://target/input2queue").log(LoggingLevel.INFO, " file is moved to queue").to("activemq:queue:testqueue");
     }
 }
